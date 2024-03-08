b15b6cecfb641bff4bc559b99d507d1b60e790ce
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2024-03-08 09:53:28 +0700

    fetch data in server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import SectionOne from "@/components/home/section.one";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch('http://localhost:8000/api/v1/products?sort=-sold&current=1&pageSize=1')
+  const result = await response.json()
+  console.log(result.data.result)
   return (
     <>
       <SectionOne />
