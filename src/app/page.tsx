000fb9e4ec4f8dc3e876fcf1dd566142adde4204
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2024-03-08 15:34:39 +0700

    define datatype response

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import SectionOne from "@/components/home/section.one";
 import { api } from "@/utils/api";
 
 export default async function Home() {
-  const categories = await api.get('categories').json();
-  console.log('hieu', categories)
+  const categories = await api.get('categories').json<IBackendResponse<ICategories[]>>();
+  console.log('hieu', categories.data[0]?.title ?? [])
   return (
     <>
       <SectionOne />
---
+++ src/types/global.d.ts
@@ -0,0 +1,21 @@
+declare global {
+    /*~ Here, declare things that go in the global namespace, or augment
+     *~ existing declarations in the global namespace
+     */
+    interface IBackendResponse<T> {
+        data?: T,
+        message: string | string[],
+        statusCode: number,
+        error?: string
+    }
+    interface ICategories {
+        "_id": string,
+        "title": string,
+        "image"?: string,
+        "slug"?: string,
+        "createdAt"?: string,
+        "updatedAt"?: string,
+        "__v"?: number
+    }
+}
+export { };
