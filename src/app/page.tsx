96c6e839e9ce80b18cb078d54254a29481e5f482
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2024-03-08 15:13:04 +0700

    ky npm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import SectionOne from "@/components/home/section.one";
+import { api } from "@/utils/api";
 
 export default async function Home() {
-  const response = await fetch('http://localhost:8000/api/v1/products?sort=-sold&current=1&pageSize=1')
-  const result = await response.json()
-  console.log(result.data.result)
+  const categories = await api.get('categories').json();
+  console.log('hieu', categories)
   return (
     <>
       <SectionOne />
---
+++ src/utils/api.ts
@@ -0,0 +1,31 @@
+import ky from 'ky';
+
+// On https://my-site.com
+
+export const api = ky.create({
+    prefixUrl: 'http://localhost:8000/api/v1',
+    // hooks: {
+    //     afterResponse: [
+    //         (_request, _options, response) => {
+    //             // You could do something with the response, for example, logging.
+    //             return response
+
+    //             // Or return a `Response` instance to overwrite the response.
+    //             // return new Response('A different response', { status: 200 });
+    //         },
+
+    //         // Or retry with a fresh token on a 403 error
+    //         async (request, options, response) => {
+    //             if (response.status === 403) {
+    //                 // Get a fresh token
+    //                 const token = await ky('https://example.com/token').text();
+
+    //                 // Retry with the token
+    //                 request.headers.set('Authorization', `token ${token}`);
+
+    //                 return ky(request);
+    //             }
+    //         }
+    //     ]
+    // }
+});
