1ebfb72147755c7d8acefe5efd6d2685364887cb
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2024-03-08 15:57:18 +0700

    add storeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,20 +4,23 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@/theme';
 import Header from '@/components/header/header';
+import { StoreProvider } from './StoreProvider';
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            <Header />
-            {props.children}
-          </ThemeProvider>
-        </AppRouterCacheProvider>
-      </body>
-    </html>
+    <StoreProvider>
+      <html lang="en">
+        <body>
+          <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+            <ThemeProvider theme={theme}>
+              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+              <CssBaseline />
+              <Header />
+              {props.children}
+            </ThemeProvider>
+          </AppRouterCacheProvider>
+        </body>
+      </html>
+    </StoreProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { api } from "@/utils/api";
 
 export default async function Home() {
   const categories = await api.get('categories').json<IBackendResponse<ICategories[]>>();
-  console.log('hieu', categories.data[0]?.title ?? [])
+  console.log('hieu', categories)
   return (
     <>
       <SectionOne />
