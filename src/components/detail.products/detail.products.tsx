135b3d4b49e7ebdd66529ccfa2de2b8359c39f27
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2024-03-12 09:37:54 +0700

    breadcrumbs component basic

--- src/components/detail.products/detail.products.tsx
+++ src/components/detail.products/detail.products.tsx
@@ -1,12 +1,37 @@
 'use client'
 import Container from "@mui/material/Container"
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+
+function handleClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    event.preventDefault();
+    console.info('You clicked a breadcrumb.');
+}
 interface IProps {
     slug: string
 }
 const DetailProducts = (props: IProps) => {
     const { slug } = props;
     return (
-        <Container>hi {slug}</Container>
+        <Container sx={{ mt: 2 }}>
+            <div role="presentation" onClick={handleClick}>
+                <Breadcrumbs aria-label="breadcrumb">
+                    <Link underline="hover" color="inherit" href="/">
+                        MUI
+                    </Link>
+                    <Link
+                        underline="hover"
+                        color="inherit"
+                        href="/material-ui/getting-started/installation/"
+                    >
+                        Core
+                    </Link>
+                    <Typography color="text.primary">Breadcrumbs</Typography>
+                </Breadcrumbs>
+            </div>
+        </Container>
     )
 }
 export default DetailProducts
