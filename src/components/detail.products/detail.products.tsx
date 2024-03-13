32d02485145c9958a41d4663da9f7c075977b77f
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2024-03-13 16:52:45 +0700

    custom react image gallery

--- src/components/detail.products/detail.products.tsx
+++ src/components/detail.products/detail.products.tsx
@@ -8,7 +8,15 @@ import { capitalizeFirstLetter } from "@/utils/helper";
 import "react-image-gallery/styles/css/image-gallery.css";
 import './style.css'
 import ImageGallery, { ReactImageGalleryItem } from "react-image-gallery";
-import { Box } from "@mui/material";
+import Box from "@mui/material/Box";
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from "@/utils/styles";
+import Button from "@mui/material/Button";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
+import ExtraInfo from "./extra.info";
 
 const images: ReactImageGalleryItem[] = [
     {
@@ -24,6 +32,78 @@ const images: ReactImageGalleryItem[] = [
         thumbnail: "https://picsum.photos/id/1019/250/150/",
     },
 ];
+
+interface INav {
+    onClick: React.MouseEventHandler<HTMLElement>,
+    disabled: boolean
+}
+
+const LeftNav = (props: INav) => {
+    const { onClick, disabled } = props
+    return (
+        <ThemeProvider theme={theme}>
+            <Button sx={{
+                borderRadius: '50%',
+                position: 'absolute',
+                top: '40%',
+                left: '0',
+                display: 'flex',
+                height: '40px',
+                minWidth: '40px',
+                zIndex: 4000,
+                alignItems: 'center',
+                justifyContent: 'center',
+                px: 0
+            }}
+                onClick={onClick} disabled={disabled} variant="contained" color="violet"><ArrowBackIosNewIcon /></Button>
+        </ThemeProvider>
+    );
+}
+const RightNav = (props: INav) => {
+    const { onClick, disabled } = props
+    return (
+        <ThemeProvider theme={theme}>
+            <Button sx={{
+                borderRadius: '50%',
+                position: 'absolute',
+                top: '40%',
+                right: '0',
+                display: 'flex',
+                height: '40px',
+                minWidth: '40px',
+                zIndex: 2,
+                alignItems: 'center',
+                justifyContent: 'center',
+                px: 0
+            }}
+                onClick={onClick} disabled={disabled} variant="contained" color="violet"><ArrowForwardIosIcon /></Button>
+        </ThemeProvider>
+    );
+}
+interface IFull {
+    onClick: React.MouseEventHandler<HTMLElement>, isFullscreen: boolean
+}
+const Fullscreen = (props: IFull) => {
+    const { onClick, isFullscreen } = props
+    return (
+        <ThemeProvider theme={theme}>
+            <Button sx={{
+                borderRadius: '50%',
+                position: 'absolute',
+                bottom: '0',
+                right: '0',
+                display: 'flex',
+                height: '40px',
+                minWidth: '40px',
+                zIndex: 2,
+                alignItems: 'center',
+                justifyContent: 'center',
+                px: 0
+            }}
+                onClick={onClick} variant="contained" color="violet">{!isFullscreen ? <FullscreenIcon /> : <FullscreenExitIcon />}</Button>
+        </ThemeProvider>
+    );
+}
 
 interface IProps {
     data: null | IProducts
@@ -46,14 +126,24 @@ const DetailProducts = (props: IProps) => {
             </Breadcrumbs>
             <Box sx={{ mt: 2, display: 'flex' }}>
                 <Box sx={{ width: '40%' }}>
-                    <ImageGallery items={images} />
+                    <ImageGallery
+                        renderLeftNav={(onClick, disabled) => (
+                            <LeftNav onClick={onClick} disabled={disabled} />
+                        )}
+                        renderRightNav={(onClick, disabled) => (
+                            <RightNav onClick={onClick} disabled={disabled} />
+                        )}
+                        renderFullscreenButton={(onClick, isFullscreen) => (
+                            <Fullscreen onClick={onClick} isFullscreen={isFullscreen} />
+                        )}
+                        showPlayButton={false} items={images} />
                 </Box>
                 <Box sx={{ width: '60%', display: 'flex' }}>
                     <Box sx={{ width: '62.5%' }}>
                         info
                     </Box>
                     <Box sx={{ width: "37.5%" }}>
-                        extra
+                        <ExtraInfo />
                     </Box>
                 </Box>
 
---
+++ src/components/detail.products/extra.info.tsx
@@ -0,0 +1,7 @@
+'use client'
+const ExtraInfo = () => {
+    return (
+        <div>extra info</div>
+    )
+}
+export default ExtraInfo
