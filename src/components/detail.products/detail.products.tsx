f80866786d436c8d0d98c203dbcc429e6d9954fd
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2024-03-13 15:30:14 +0700

    react-image-gallery

--- src/components/detail.products/detail.products.tsx
+++ src/components/detail.products/detail.products.tsx
@@ -5,6 +5,25 @@ import Typography from '@mui/material/Typography';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from "next/link";
 import { capitalizeFirstLetter } from "@/utils/helper";
+import "react-image-gallery/styles/css/image-gallery.css";
+import './style.css'
+import ImageGallery, { ReactImageGalleryItem } from "react-image-gallery";
+import { Box } from "@mui/material";
+
+const images: ReactImageGalleryItem[] = [
+    {
+        original: "https://picsum.photos/id/1018/1000/600/",
+        thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+        original: "https://picsum.photos/id/1015/1000/600/",
+        thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+        original: "https://picsum.photos/id/1019/1000/600/",
+        thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+];
 
 interface IProps {
     data: null | IProducts
@@ -25,7 +44,20 @@ const DetailProducts = (props: IProps) => {
                 </Link>
                 <Typography color="text.primary">{data?.title || 'Product'}</Typography>
             </Breadcrumbs>
-            {/* <RouterBreadcrumbs /> */}
+            <Box sx={{ mt: 2, display: 'flex' }}>
+                <Box sx={{ width: '40%' }}>
+                    <ImageGallery items={images} />
+                </Box>
+                <Box sx={{ width: '60%', display: 'flex' }}>
+                    <Box sx={{ width: '62.5%' }}>
+                        info
+                    </Box>
+                    <Box sx={{ width: "37.5%" }}>
+                        extra
+                    </Box>
+                </Box>
+
+            </Box>
         </Container>
     )
 }
