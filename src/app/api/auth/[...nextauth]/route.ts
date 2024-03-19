bb4db578fd5e5aa8e92cb51d9c3ebbb424259bf0
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2024-03-19 13:30:55 +0700

    config github providers

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,21 @@
+import NextAuth, { AuthOptions } from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+export const authOptions: AuthOptions = {
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
+        }),
+    ],
+    callbacks: {
+        async jwt({ token, user, account, profile }) {
+            console.log('hi', token, user, account, profile)
+            return token
+
+        }
+    }
+}
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
