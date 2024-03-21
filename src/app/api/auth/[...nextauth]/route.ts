2057db6f7a0057b069b89c2cb5c74bb6e277f8ad
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2024-03-21 17:01:42 +0700

    assign token properties

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,6 @@
+import { api } from "@/utils/api"
 import NextAuth, { AuthOptions } from "next-auth"
+import { JWT } from "next-auth/jwt"
 import GithubProvider from "next-auth/providers/github"
 
 export const authOptions: AuthOptions = {
@@ -12,15 +14,26 @@ export const authOptions: AuthOptions = {
     callbacks: {
         async jwt({ token, account, user, trigger }) {
             if (trigger === 'signIn' && account?.provider !== 'credentials') {
-                token.access_token = 'fake token'
-                token.refresh_token = 'fake token'
-
+                const response = await api.post(`auth/providers`, { json: { type: 'GITHUB', username: user.email } }).json<IBackendResponse<JWT>>()
+                if (response.data) {
+                    token.access_token = response.data.access_token
+                    token.refresh_token = response.data.refresh_token
+                    token.user = response.data.user
+                    token.email = user.email
+                    token.name = user.name
+                    token.picture = user.image
+                }
             }
             return token
         },
-        async session({ session, user, token }) {
-            if (session) {
+        async session({ session, token }) {
+            if (token) {
                 session.access_token = token.access_token
+                session.refresh_token = token.refresh_token
+                session.user = token.user
+                session.user.name = token.name
+                session.user.email = token.email
+                session.user.image = token.picture
             }
             return session
         },
